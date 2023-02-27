088bda436e1905c80a79771621ee15b217332abd
PRAVEENSRINI2105 <123566171+PRAVEENSRINI2105@users.noreply.github.com>
2023-02-27 17:57:37 +0530

    Add files via upload

---
+++ webstorge/sessionStorage.js
@@ -0,0 +1,49 @@
+//localstorage
+// localStorage.setItem("hello","world");
+
+// localStorage.getItem("hello")
+
+// localStorage.removeItem("hello")
+
+// localStorage.clear()
+
+// const user = {name:"Praveen"}
+// localStorage.setItem("user",user);
+
+// String(user)
+
+// localStorage.getItem("user")
+
+// localStorage.setItem("user-copy", JSON.stringify(user));
+
+// localStorage.getItem("user-copy")
+
+// JSON.parse(localStorage.getItem("user-copy"));
+
+
+// function setLocaltorageData("user"){JSON}
+
+
+
+//Sessionstorage
+
+
+// sessionStorage.setItem("hello","world");
+
+// sessionStorage.getItem("hello")
+
+// sessionStorage.removeItem("hello")
+
+// sessionStorage.clear()
+
+
+//cookie
+
+document.cookie = 'name=praveen; expires =' + new Date(2024,0,1)
+.toUTCString()
+
+
+// document.cookie = 'lastname=balaji; expires =' + new Date(9999,0,1)
+// .toUTCString()
+
+// console.log(document.cookie)
